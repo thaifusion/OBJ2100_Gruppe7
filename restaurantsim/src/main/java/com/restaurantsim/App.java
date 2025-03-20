aeb6adc5b2b7ca4696bcdc5983a7bb29a97ba9fd
JHTgh <jorgenthorjussen@live.com>
2025-03-20 15:42:43 +0100

    Mapperens
    Renset mapper for unødvendige filer, og slettet innhold i App.java.

--- restaurantsim/src/main/java/com/restaurantsim/App.java
+++ restaurantsim/src/main/java/com/restaurantsim/App.java
@@ -1,9 +1,8 @@
 package com.restaurantsim;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,22 +12,14 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    private BorderPane pane = new BorderPane();
+    private Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
--- restaurantsim/src/main/java/module-info.java
+++ restaurantsim/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.restaurantsim {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.restaurantsim to javafx.fxml;
     exports com.restaurantsim;
